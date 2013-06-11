0c9aec7b6adc4faf841feba4e010b63643777321
Bartuz <iamnotawowplayer@gmail.com>
2013-06-11 14:59:28 -0400

    Modify method now returns nil for unknon contact

--- crm_database.rb
+++ crm_database.rb
@@ -14,11 +14,12 @@ class Database
 
 	def modify_contact(selection, attribute, new_value)
 		@contacts_array.each do |contact|
-  		if (contact.id == selection) || (contact.firstname == selection) || (contact.lastname == selection) || (contact.email == selection) || (contact.notes == selection)
+  			if (contact.id == selection) || (contact.firstname == selection) || (contact.lastname == selection) || (contact.email == selection) || (contact.notes == selection)
 	  	# instance_variables.map(&method(:instance_variable_get))#.any?{ |value| value == selection }
 		    contact.send(attribute.to_s + "=", new_value)
-		    return contact
-		  end
-  	end 
-  end
+		    	return contact
+		  	end
+  		end 
+  		nil
+  	end
 end
